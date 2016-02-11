939a1ca3aa5e1698ab81ef18666110c0772492e0
733amir@gmail.com <733amir@gmail.com>
2016-02-11 16:46:30 +0330

    Making a node pass all energy to a new node, if no new node send some energy to some neighbour. Defining is_safe()

--- src/AI.py
+++ src/AI.py
@@ -1,17 +1,37 @@
 __author__ = 'AmirHS'
 
-from random import randint
+from random import randint, choice
 
+# Defiles the Zone where a dragon can distribute it's power without danger
+safe_zone_len = 3
 
 class AI():
     def do_turn(self, world):
-        # fill this method, we've presented a stupid AI for example!
-        my_nodes = world.my_nodes
-        for source in my_nodes:
-            # get neighbours
-            neighbours = source.neighbours
-            if len(neighbours) > 0:
-                # select a random neighbour
-                destination = neighbours[randint(0, len(neighbours) - 1)]
-                # move half of the source's army to the neighbour node
-                world.move_army(source, destination, int(source.army_count/2))
+        # AI Attributes
+        self.world = world
+        for source in world.my_nodes:
+            all_neighbours_are_mine = True
+            for neighbour in source.neighbours:
+                if neighbour.owner != world.my_id:
+                    world.move_army(source, neighbour, source.army_count)
+                    all_neighbours_are_mine = False
+                    break
+            if all_neighbours_are_mine:
+                world.move_army(source, choice(source.neighbours), source.army_count // len(source.neighbours))
+
+    def is_safe(self, node):
+        neighbours = node.neighbours
+        neighbours_of_neighbours = []
+        is_there_enemy = False
+        for i in range(safe_zone_len):
+            for neighbour in neighbours:
+                if neighbour.owner == int(not bool(self.world.my_id)):
+                    is_there_enemy = True
+                    break
+                neighbours_of_neighbours.append(neighbour)
+            if is_there_enemy:
+                break
+            else:
+                neighbours = neighbours_of_neighbours
+                neighbours_of_neighbours = []
+        return False if is_there_enemy else True
